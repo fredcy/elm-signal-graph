a416e672bc6f60873ab3442384cb195b27d3ff47
Fred Yankowski <fcy@imsa.edu>
2016-04-13 09:19:53 -0500

    baseline

---
+++ Main.elm
@@ -0,0 +1,59 @@
+module Main (..) where
+
+import Html
+import Html.Attributes
+import Html.Events exposing (on, targetValue)
+import Task
+import Effects
+import StartApp
+
+
+type alias Model =
+  String
+
+
+init : ( Model, Effects.Effects Action )
+init =
+  ( "", Effects.none )
+
+
+type Action
+  = NoOp
+  | Update String
+
+
+update : Action -> Model -> ( Model, Effects.Effects Action )
+update action model =
+  case action of
+    NoOp ->
+      ( model, Effects.none )
+    Update str ->
+      ( str, Effects.none )
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+  Html.div
+    []
+    [ Html.text "Hello!"
+    , Html.input [ on "input" targetValue (Signal.message address << Update) ] []
+    ]
+
+
+app : StartApp.App Model
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+
+port tasks : Signal (Task.Task Effects.Never ())
+port tasks =
+  app.tasks
+
+
+main : Signal Html.Html
+main =
+  app.html
